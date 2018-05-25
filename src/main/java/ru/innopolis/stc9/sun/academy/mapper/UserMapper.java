eb2a6aaea07c235feff02e519cae3d4eea26f39b
Albert Iskhakov <aliskhakov@gmail.com>
2018-05-25 16:55:44 +0300

    Добавил маппер для сущности пользователя.
    Ref SUN-2

---
+++ src/main/java/ru/innopolis/stc9/sun/academy/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package ru.innopolis.stc9.sun.academy.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.innopolis.stc9.sun.academy.dto.UserDTO;
+import ru.innopolis.stc9.sun.academy.entity.User;
+
+@Component
+public class UserMapper {
+    UserMapper() {
+    }
+
+    public static UserDTO toDto(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setPatronymic(user.getPatronymic());
+        return userDTO;
+    }
+
+    public static User toEntity(UserDTO userDTO) {
+        User user = new User(userDTO.getId());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setPatronymic(userDTO.getPatronymic());
+        return user;
+    }
+}
