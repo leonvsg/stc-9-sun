0ef9644dac59ae3cdfbe2acb1504731db917156f
s.vazhorov <leonvsg@gmail.com>
2018-05-27 23:32:55 +0300

    Добавил методы для обработки пользователя

--- src/main/java/ru/innopolis/stc9/sun/academy/controllers/UsersController.java
+++ src/main/java/ru/innopolis/stc9/sun/academy/controllers/UsersController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.innopolis.stc9.sun.academy.dto.UserDTO;
 import ru.innopolis.stc9.sun.academy.service.UserService;
 import javax.validation.Valid;
@@ -18,6 +15,11 @@ import javax.validation.Valid;
 public class UsersController {
     private static final Logger LOGGER = Logger.getLogger(UsersController.class);
     private static final String TITLE = "Пользователи";
+    private static final String TITLE_ATTRIBUTE_NAME = "title";
+    private static final String USERS_VIEW_NAME = "users";
+    private static final String USER_VIEW_NAME = "user";
+    private static final String USER_ATTRIBUTE_NAME = "user";
+    private static final String USERS_ATTRIBUTE_NAME = "users";
     private final UserService userService;
 
     @Autowired
@@ -27,21 +29,42 @@ public class UsersController {
 
     @GetMapping
     public String getUsers(ModelMap model){
-        model.addAttribute("title", TITLE);
-        model.addAttribute("users", userService.getUsers());
-        return "users";
+        model.addAttribute(TITLE_ATTRIBUTE_NAME, TITLE);
+        model.addAttribute(USERS_ATTRIBUTE_NAME, userService.getUsers());
+        return USERS_VIEW_NAME;
     }
 
-    @PostMapping("/add")
-    public String addUser(@Valid @ModelAttribute("user") final UserDTO user,
+    @PostMapping
+    public String addUser(@Valid @ModelAttribute(USER_ATTRIBUTE_NAME) final UserDTO user,
                            BindingResult bindingResult,
                            ModelMap model){
         if (!bindingResult.hasErrors()) {
             userService.addUser(user);
             return "redirect:/users";
         } else {
-            model.addAttribute("title", TITLE);
-            return "users";
+            model.addAttribute(TITLE_ATTRIBUTE_NAME, TITLE);
+            return USERS_VIEW_NAME;
         }
+    }
+
+    @GetMapping("/{id}/")
+    public String getUser(@PathVariable Integer id, ModelMap model){
+        model.addAttribute(TITLE_ATTRIBUTE_NAME, TITLE);
+        model.addAttribute(USER_ATTRIBUTE_NAME, userService.getUserById(id));
+        return USER_VIEW_NAME;
+    }
+
+    @PostMapping("/{id}/")
+    public String updateUser(@PathVariable Integer id,
+                             @Valid @ModelAttribute(USER_ATTRIBUTE_NAME) final UserDTO user,
+                             BindingResult bindingResult,
+                             ModelMap model){
+        if (!bindingResult.hasErrors()) {
+            user.setId(id);
+            userService.updateUser(user);
+        } else {
+            model.addAttribute(TITLE_ATTRIBUTE_NAME, user.getFullName());
+        }
+        return USER_VIEW_NAME;
     }
 }
